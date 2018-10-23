f446056558fb331ea461ed4f0dcedd37c91b5916
pauloizd <paulozp1995@gmail.com>
2018-10-23 13:45:07 -0300

    adicionado arquivos spring do projeto e configurado arquivo application.properties apontando pro banco iftube

---
+++ src/main/java/com/wep/iftube/IftubeApplication.java
@@ -0,0 +1,12 @@
+package com.wep.iftube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IftubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IftubeApplication.class, args);
+	}
+}
